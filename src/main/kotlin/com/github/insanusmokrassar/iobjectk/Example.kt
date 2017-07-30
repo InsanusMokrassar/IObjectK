164b61e32e723e1c88d63b10702e7afb4b7d1c08
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-07-30 23:19:27 +0600

    add properties file config

--- src/main/kotlin/com/github/insanusmokrassar/iobjectk/Example.kt
+++ src/main/kotlin/com/github/insanusmokrassar/iobjectk/Example.kt
@@ -1,7 +1,18 @@
 package com.github.insanusmokrassar.iobjectk
 
+import java.io.FileInputStream
 import java.util.*
+import java.util.logging.LogManager
 
 fun main(args: Array<String>) {
     println(Arrays.toString(args))
+}
+
+/**
+ * Need "LOGGER_CONFIG_PATH" environment variable which set the place where was put .properties file with config of logger
+ */
+fun loadLoggerConfig() {
+    FileInputStream(System.getenv("LOGGER_CONFIG_PATH")).use {
+        LogManager.getLogManager().readConfiguration(it)
+    }
 }
