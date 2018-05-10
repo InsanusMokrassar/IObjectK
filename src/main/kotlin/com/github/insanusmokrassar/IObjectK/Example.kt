237dbddd6234ce8ea79727ad7a71151b3bf9888e
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2018-05-10 13:19:28 +0800

    add duplicate operation

--- src/main/kotlin/com/github/insanusmokrassar/IObjectK/Example.kt
+++ src/main/kotlin/com/github/insanusmokrassar/IObjectK/Example.kt
@@ -1,7 +1,19 @@
 package com.github.insanusmokrassar.IObjectK
 
-import java.util.*
+import com.github.insanusmokrassar.IObjectK.extensions.duplicate
+import com.github.insanusmokrassar.IObjectK.realisations.SimpleIObject
 
 fun main(args: Array<String>) {
-    println(Arrays.toString(args))
+    SimpleIObject().apply {
+        set("example1", SimpleIObject().apply { this["example1.2"] = 1.2 })
+        set("example2", listOf("2.1", 2.2, true))
+        println(this.apply {
+            println(hashCode())
+            println(get<Any>("example1").hashCode())
+        })
+        println(this.duplicate().apply {
+            println(hashCode())
+            println(get<Any>("example1").hashCode())
+        })
+    }
 }
--- src/main/kotlin/com/github/insanusmokrassar/IObjectK/extensions/IInputObject.kt
+++ src/main/kotlin/com/github/insanusmokrassar/IObjectK/extensions/IInputObject.kt
@@ -1,9 +1,8 @@
 package com.github.insanusmokrassar.IObjectK.extensions
 
 import com.github.insanusmokrassar.IObjectK.exceptions.ReadException
-import com.github.insanusmokrassar.IObjectK.interfaces.IInputObject
-import com.github.insanusmokrassar.IObjectK.interfaces.has
-import com.github.insanusmokrassar.IObjectK.realisations.StandardIInputObjectIterator
+import com.github.insanusmokrassar.IObjectK.interfaces.*
+import com.github.insanusmokrassar.IObjectK.realisations.*
 
 /**
  * Creating map to IInputObject
@@ -14,6 +13,42 @@ fun <K, V> IInputObject<K, V>.asMap(): Map<K, V> {
 
 fun <K, V> IInputObject<K, V>.iterator(): Iterator<Pair<K, V>> {
     return StandardIInputObjectIterator(this)
+}
+
+private fun <V> List<V>.duplicate(): List<V> {
+    return ArrayList<V>().also {
+        newList ->
+        forEach {
+            value ->
+            when(value) {
+                is List<*> -> ((value as? List<V>) ?. duplicate() as? V)
+                is IInputObject<*, *> -> (value.duplicate() as? V)
+                else -> value // don't know how to copy
+            } ?.let {
+                newList.add(it)
+            }
+        }
+    }
+}
+
+/**
+ * Safe recursive copy all data from IInputObject to new object
+ */
+fun <K, V> IInputObject<K, V>.duplicate(): CommonIObject<K, V> {
+    return SimpleCommonIObject<K, V>().also {
+        newObject ->
+        iterator().forEach {
+            val value = it.second
+            when(value) {
+                is List<*> -> ((value as? List<V>) ?. duplicate() as? V)
+                is IInputObject<*, *> -> (value.duplicate() as? V)
+                else -> value // don't know how to copy
+            } ?.let {
+                copiedValue ->
+                newObject[it.first] = copiedValue
+            }
+        }
+    }
 }
 
 /**
